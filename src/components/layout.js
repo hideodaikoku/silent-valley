a1e4f90bb95f28a48de0b44fdb817df950bf618a
Hideo Daikoku <neildaikoku@gmail.com>
2025-04-28 20:43:57 +0900

    update analytics

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { Analytics } from "@vercel/analytics/react"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -22,6 +23,7 @@ const Layout = ({ location, title, children }) => {
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
+      <Analytics/>
       <footer>
         © {new Date().getFullYear()}
         {` `}
